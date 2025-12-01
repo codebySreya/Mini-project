5da3bdf4629d735f2ed684fc8cf7e1886ad9a427
codebySreya <sreyapraveenlal4444@gmail.com>
2025-12-01 19:50:04 +0530

    Add files via upload

--- employee details assignmentmysql.sql
+++ employee details assignmentmysql.sql
@@ -1,7 +1,13 @@
 CREATE  DATABASE Employee;
 USE Employee;
-CREATE TABLE Department(id INT,name VARCHAR(50));
-CREATE TABLE Location(id INT,Location VARCHAR(30));
+CREATE TABLE departments (
+    department_id INT PRIMARY KEY,
+    name VARCHAR(100)
+);
+CREATE TABLE locations (
+    location_id INT PRIMARY KEY,
+    location_name VARCHAR(100)
+);
 CREATE TABLE Employees(employee_id INT,employee_name VARCHAR(50),gender enum('M','F'),age INT,hire_Data DATE,designation VARCHAR(100),Department_id INT,Location_id INT,salary Decimal(10,2));
 ALTER TABLE Employees ADD column email VARCHAR(100);
 ALTER TABLE Employees MODIFY COLUMN designation VARCHAR(255);
@@ -29,3 +35,76 @@ CREATE TABLE Employees (
     FOREIGN KEY (Department_ID) REFERENCES Department(id),
     FOREIGN KEY (Location_ID) REFERENCES Location(id)
 );
+select distinct salary
+from employees;
+select name,age as employee_age,salary as employee_salary from employees;
+select*from employees
+where salary >50000 and hire_date <'2016-01-01';
+set sql_safe_updates=0;
+update employees
+set designation='data scientist'
+where designation is null;
+select*from employees
+order by department_id asc,salary desc;
+select*from employees
+where year(hire_date)=2018
+order by hire_date
+limit 5;
+select sum(salary)as total_salary
+from employees
+where department_id=(
+select department_id
+from departments
+where name ='finance');
+select min(age) as minimum_age from employees;
+INSERT INTO locations (location_id, location_name)
+VALUES (1, 'New York'), (2, 'London'), (3, 'Mumbai');
+select l.location_name ,max(e.salary) as max_salary from
+employees e join locations l on e.location_id=l.location_id
+group by l.location_name;
+
+SELECT 
+    Designation,
+    AVG(Salary) AS Average_Salary
+FROM Employees
+WHERE Designation LIKE '%Analyst%'
+GROUP BY Designation;
+
+
+SELECT
+    Department_ID,
+    COUNT(*) AS Total_Employees
+FROM Employees
+GROUP BY Department_ID
+HAVING COUNT(*) < 3;
+    SELECT 
+    Location_ID,
+    AVG(Age) AS Avg_Age
+FROM Employees
+WHERE Gender = 'F'
+GROUP BY Location_ID
+HAVING AVG(Age) < 30;
+SELECT 
+e.name as employee_name,
+e.designation,
+d.name as department_name,
+e.location_id
+FROM Employees e
+INNER JOIN Department D
+ON e.Department_ID= d.ID;
+select
+e.name as employee_name,
+d.name as department_name
+from employees e
+right join department d
+on e.department_id=d.id;
+
+
+
+select * from employees;
+
+
+
+
+
+
